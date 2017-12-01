2166f036151d227e614d1b9889e57c17425590a0
juarezadolfo <juarezadolfo@hotmail.com>
2017-12-01 13:01:00 -0800

    final commit

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -11,52 +11,92 @@ console.log(randomNum);
 $(".well-lg").html(randomNum);
 
 
+// random numbers for each jewel - random number has to be between 1 - 12
+
+var num1= Math.floor(Math.random()*11+1)
+var num2= Math.floor(Math.random()*11+1)
+var num3= Math.floor(Math.random()*11+1)
+var num4= Math.floor(Math.random()*11+1)
+
+  var userTotal= 0; 
+  var wins= 0;
+  var losses = 0;
+  //  variables for wins & losses
+$('#numberWins').text(wins);
+$('#numberLosses').text(losses);
+//reset the game
+function reset(){
+      Random=Math.floor(Math.random()*101+19);
+      console.log(Random)
+      $('#randomNumber').text(Random);
+      num1= Math.floor(Math.random()*11+1);
+      num2= Math.floor(Math.random()*11+1);
+      num3= Math.floor(Math.random()*11+1);
+      num4= Math.floor(Math.random()*11+1);
+      userTotal= 0;
+      $('#finalTotal').text(userTotal);
+      } 
+//adds to the wins
+function winner(){
+alert("You won!");
+  wins++; 
+  $('#numberWins').text(wins);
+  reset();
+}
+//adds to the losses
+function loser(){
+alert ("You lose!");
+  losses++;
+  $('#numberLosses').text(losses);
+  reset()
+}
+//sets up click for jewels
+$('#one').on ('click', function(){
+    userTotal = userTotal + num1;
+    console.log("New userTotal= " + userTotal);
+    $('#finalTotal').text(userTotal); 
+          
+        if (userTotal == Random){
+          winner();
+        }
+        else if ( userTotal > Random){
+          loser();
+        }   
+  })  
+  $('#two').on ('click', function(){
+    userTotal = userTotal + num2;
+    console.log("New userTotal= " + userTotal);
+    $('#finalTotal').text(userTotal); 
+        if (userTotal == Random){
+          winner();
+        }
+        else if ( userTotal > Random){
+          loser();
+        } 
+  })  
+  $('#three').on ('click', function(){
+    userTotal = userTotal + num3;
+    console.log("New userTotal= " + userTotal);
+    $('#finalTotal').text(userTotal);
+          if (userTotal == Random){
+          winner();
+        }
+        else if ( userTotal > Random){
+          loser();
+        } 
+  })  
+  $('#four').on ('click', function(){
+    userTotal = userTotal + num4;
+    console.log("New userTotal= " + userTotal);
+    $('#finalTotal').text(userTotal); 
+      
+          if (userTotal == Random){
+          winner();
+        }
+        else if ( userTotal > Random){
+          loser();
+        }
+  }); 
+
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// // basic function structure
-// function hello(name){
-//     alert("Hello" + name);
-
-// }
-// // this is calling the function above
-// hello("connor");
-// hello("kevin");
-// hello("adolfo");
-
-// // array example
-// var studentArray = ["Kevin", "John", "Kyle"];
-// console.log(studentArray.length);
-
-// for (var i = 0; i < studentArray.length; i++){
-//     console.log(studentArray[i]);
-//     if(studentArray[i] === "Kevin"){
-//         alert ("Got" + studentArray[i]);
-//      } else if (studentArray[i] === "Kyle"){
-//             alert ("got" + studentArray[i]);
-//         }   else {
-//                 alert("Not Kevin");
-//             }
-// }
-
-
-var redCrystal = "button1"
-var blueCrystal = "button2"
-var greenCrystal = "button3"
-var yellowCrystal = "button4"
-
-
 
